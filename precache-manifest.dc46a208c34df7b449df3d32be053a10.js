79e2d35d4ce2d8ecaf5a079ea68fceccd2542d1c
Alexandre Disdier <28678511+alexdisdier@users.noreply.github.com>
2019-08-04 22:34:07 +0200

    Updates

---
+++ precache-manifest.dc46a208c34df7b449df3d32be053a10.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b668c0beb5b2db3a8a325cd94d1a904",
+    "url": "/todo-client/index.html"
+  },
+  {
+    "revision": "cc337728142a39e0ed6f",
+    "url": "/todo-client/static/css/main.72edbfb0.chunk.css"
+  },
+  {
+    "revision": "a0c4ed407a9722109884",
+    "url": "/todo-client/static/js/2.25c26266.chunk.js"
+  },
+  {
+    "revision": "cc337728142a39e0ed6f",
+    "url": "/todo-client/static/js/main.a288c4a7.chunk.js"
+  },
+  {
+    "revision": "8584d38ece7afd159171",
+    "url": "/todo-client/static/js/runtime~main.152da5ec.js"
+  },
+  {
+    "revision": "f380166769b3c7623a0114ccecc6036b",
+    "url": "/todo-client/static/media/Cloud_Warning.f3801667.svg"
+  },
+  {
+    "revision": "14ca361262f935be9c9adc735422d991",
+    "url": "/todo-client/static/media/loading.14ca3612.svg"
+  }
+]);
