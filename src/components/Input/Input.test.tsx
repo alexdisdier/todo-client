598e387a62be5c19f3ba82843c6a8d2a62e29e86
Alexandre Disdier <28678511+alexdisdier@users.noreply.github.com>
2019-09-17 22:04:56 +0200

    adding more tests

--- src/components/Input/Input.test.tsx
+++ src/components/Input/Input.test.tsx
@@ -1,50 +1,30 @@
 import React from "react";
-import { shallow, configure, mount } from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
-import ReactDOM from "react-dom";
+import { shallow } from "enzyme";
 
 import Input from "./Input";
 
-/*  
-  We're using Jest as our test runner. It will provide the describe, it and expect and toEqual functions.
-  We're using Enzyme for the shallow wrapper which gives us the method find
-*/
+describe("Input", () => {
+  let props: any;
 
-configure({ adapter: new Adapter() });
-
-describe("<Input /> rendering", () => {
-  it("renders without crashing", () => {
-    const div = document.createElement("div");
-    ReactDOM.render(<Input />, div);
-    ReactDOM.unmountComponentAtNode(div);
+  beforeEach(() => {
+    props = {
+      name: "name",
+      value: "value",
+      handleChange: jest.fn()
+    };
   });
 
-  it("should render an input area", () => {
-    const wrapper = shallow(<Input />);
-    expect(wrapper.find("input").length).toEqual(1);
-  });
-
-  it("should render a placeholder", () => {
-    const placeholder_text = "type some text here";
-    const wrapper = shallow(<Input placeholder={placeholder_text} />);
-    expect(wrapper.prop("placeholder")).toEqual(placeholder_text);
-  });
-
-  it("should render the correct type", () => {
-    const type = "text";
-    const wrapper = shallow(<Input type={type} />);
-    expect(wrapper.prop("type")).toEqual(type);
+  it("renders correctly", () => {
+    const wrapper = shallow(<Input {...props} />);
+    expect(wrapper).toMatchInlineSnapshot(`
+      <input
+        className="card-task"
+        name="name"
+        onChange={[MockFunction]}
+        placeholder="type some text here"
+        type="text"
+        value="value"
+      />
+    `);
   });
 });
-
-// describe("<Input /> UI interactions", () => {
-//   it("should call onChange prop with input value", () => {
-//     const wrapper = shallow(<Input />);
-//     wrapper.find("input").simulate("change", {
-//       target: {
-//         value: "New Comment"
-//       }
-//     });
-//     expect(wrapper.prop("value")).toEqual("New Comment");
-//   });
-// });
